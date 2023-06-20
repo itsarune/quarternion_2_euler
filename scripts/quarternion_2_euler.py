b7fc2ce26d6b54b16f5285bb1aa13d8b9f21f89c
itsarune <arun.sfc@outlook.com>
2023-06-20 12:11:38 -0700

    working

--- scripts/quarternion_2_euler.py
+++ scripts/quarternion_2_euler.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import rospy
 
-from std_msgs import Float64
-from sensor_msgs import Imu
-from tf_transformattions import euler_from_quarternion
+from std_msgs.msg import Float64
+from sensor_msgs.msg import Imu
+import tf
 
 class quarternion_2_euler:
     def __init__(self):
@@ -12,11 +12,11 @@ class quarternion_2_euler:
         self.roll_pub = rospy.Publisher('/roll', Float64, queue_size=1)
         self.pitch_pub = rospy.Publisher('/pitch', Float64, queue_size=1)
         self.yaw_pub = rospy.Publisher('/yaw', Float64, queue_size=1)
-        rospy.Subscriber('/imu/data_raw', Imu, self.convert_2_euler)
+        rospy.Subscriber('/imu/data', Imu, self.convert_2_euler)
 
     def convert_2_euler(self, imu_msg):
-        (roll, pitch, yaw) = euler_from_quarternion(imu_msg.orientation.x, imu_msg.orientation.y, \
-                imu_msg.orientation.z, imu_msg.orientation.w)
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([imu_msg.orientation.x, imu_msg.orientation.y, \
+                imu_msg.orientation.z, imu_msg.orientation.w])
         self.roll_pub.publish(roll)
         self.pitch_pub.publish(pitch)
         self.yaw_pub.publish(yaw)
